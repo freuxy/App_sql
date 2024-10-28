f7aaf85a52f3a0ece04c0397490bbb02f94f3961
bsessou <bignon-boris-freuxy.sessou@etudiant.univ-rennes1.fr>
2024-10-28 15:14:33 +0100

    Selectbox

--- app.py
+++ app.py
@@ -7,13 +7,21 @@ import duckdb
 
 st.write("SQL coach vous accompagne dans la révision de vos requêtes")
 
-try:
-  user_input=st.text_area(label="Veuillez saisir votre requête")
-  df = pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-  })
 
+
+option = st.selectbox(
+  "Quelle notion voulez-vous apprendre?",
+  ("Groupy", "CTE", "Windows functions"),
+)
+st.write("You selected:", option)
+
+user_input=st.text_area(label="Veuillez saisir votre requête")
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+try:
   res = duckdb.sql(user_input).df()
   st.write(res)
 except Exception as e:

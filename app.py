3fb211a5c5dc6836f2689c92fb0f5624635f66f3
bsessou <bignon-boris-freuxy.sessou@etudiant.univ-rennes1.fr>
2024-11-01 11:41:58 +0100

    Compare results

--- app.py
+++ app.py
@@ -25,12 +25,13 @@ muffin,3
 
 food_items = pd.read_csv(io.StringIO(csv2))
 
-answer= """
+answer_df= """
 SELECT *
 FROM beverages
 CROSS JOIN food_items
 
 """
+solution_df=duckdb.sql(answer_df).df()
 st.write("SQL coach vous accompagne dans la révision de vos requêtes")
 
 
@@ -40,14 +41,32 @@ if query:
   res = duckdb.sql(query).df()
   st.dataframe(res)
 
+  try:
+    res=res[solution_df.columns]
+    st.dataframe(res.compare(solution_df))
+  except KeyError:
+    st.write("Les colonnes ne sont pas dans le bon ordre")
+
+
+  if len(res.columns) != len(solution_df.columns):
+    st.write("Votre resultat n'a pas le même nombre de colonnes que la solution souhaitée")
+  if res.shape[0]!= solution_df.shape[0]:
+    dif=res.shape[0] - solution_df.shape[0]
+    st.write(f"Votre resultat n'a pas le même nombre de ligne que la solution souhaitée. Il vous manque {dif} lignes ")
+
+
+
+
+
+
 with st.sidebar:
   option = st.selectbox(
     "Quelle notion voulez-vous apprendre?",
     ("Groupy", "CTE", "Windows functions"),
   )
-  st.write("You selected:", option)
+  st.write("Vous avez choisi:", option)
 
-tab2, tab3 = st.tabs(["Tables", "Solutions"])
+tab2, tab3 = st.tabs(["Tables", "solution_dfs"])
 
 with tab2:
   st.write("Table beverages")
@@ -55,7 +74,7 @@ with tab2:
   st.write("Table food_items")
   st.write(food_items)
   st.write("Expected")
-  st.write(duckdb.sql(answer).df())
+  st.write(duckdb.sql(answer_df).df())
 
 with tab3:
-  st.write(answer)
+  st.write(answer_df)

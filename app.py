1027dab65d852fdb4b0fb0db80bf616d92cb628b
bsessou <bignon-boris-freuxy.sessou@etudiant.univ-rennes1.fr>
2024-11-03 21:56:15 +0100

    Fix init_db

--- app.py
+++ app.py
@@ -16,8 +16,8 @@ if "data" not in os.listdir():
     os.mkdir("data")
 
 if "exo_sql.duckdb" not in os.listdir("data"):
-    exec(open("init_db.py").read())
-    # subprocess.run(["python", "init_db.py"])
+    #exec(open("init_db.py").read())
+    subprocess.run(["python", "init_db.py"])
 
 con = duckdb.connect(database="data/exo_sql.duckdb", read_only=False)
 
